7ebaf096fa9a64923359cc5cd70978dd9a75f1bf
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2022-07-12 13:12:33 +0000

    Exercise done

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const userName = document.querySelector('#user-name');
 
 const mirrorButton = document.querySelector('#mirror-button')
+const userOutput = document.querySelector('#user-output')
 
-function getUserInput(){
-  console.log(userName.value)
+function getMirrorInput(){
+  var userInput = userName.value;
+  var splittedUserInput = userInput.split('')
+  console.log(splittedUserInput)
+  let i=0,j=splittedUserInput.length-1;
+  while(i<=j){
+    var temp = splittedUserInput[i];
+    splittedUserInput[i] = splittedUserInput[j];
+    splittedUserInput[j] = temp;
+  }
+  var output = splittedUserInput.join('')
+  console.log(output)
 }
 
-mirrorButton.addEventListener('click', getUserInput)
+mirrorButton.addEventListener('click', getMirrorInput)
+
+userName.addEventListener('input',()=>{
+  userOutput.innerText = userName.value;
+})
