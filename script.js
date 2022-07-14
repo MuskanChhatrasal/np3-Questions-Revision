bbfb040d85480c6b2e7a854eb9dd8b8c4dd26881
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2022-07-14 00:52:11 +0000

    12th April Exercise 2 - Done

--- script.js
+++ script.js
@@ -27,3 +27,29 @@ mirrorButton.addEventListener('click', ()=>{
 // userName.addEventListener('input',()=>{
 //   userOutput.innerText = getMirrorInput();
 // })
+
+// Question 2: Consider the data below. Get the total quantity of jeans and tshirt in an object.
+
+const arr = [
+    { category: "men", color: "blue", quantity: 5, item: "jeans" },
+    { category: "women", color: "blue", quantity: 5, item: "jeans" },
+    { category: "women", color: "blue", quantity: 6, item: "tshirt" },
+    { category: "men", color: "blue", quantity: 2, item: "tshirt" }
+  ];
+
+var totalQuantity = {jeansQuantity: 0, tshirtQuantity: 0}
+
+function getTotalQuantity(acc, curr){
+  if(curr.item === "jeans"){
+    return {...acc, jeansQuantity: acc.jeansQuantity + curr.quantity}
+  }else{
+    return {...acc, tshirtQuantity: acc.tshirtQuantity + curr.quantity}
+  }
+}
+
+console.log(arr.reduce(getTotalQuantity, totalQuantity))
+// Expected Output:
+// {
+// 	jeansQuantity: 10, 
+// 	tshirtQuantity: 8
+// }
